bdbfbfab6719f85cc3c343e71ca0a5bdcad17384
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2018-11-26 17:35:03 -0500

    Add register backend functionality. Add crypto for passwords. Add SQL user schema.

---
+++ public/scripts/register.js
@@ -0,0 +1,55 @@
+$('#register-button').click(function () {
+  const firstName = $('#first-name')[0].value;
+  const lastName = $('#last-name')[0].value;
+  const email = $('#email')[0].value;
+  const password = $('#password')[0].value;
+  const confirmPassword = $('#confirm-password')[0].value;
+
+  var inputs = [firstName, lastName, email, password, confirmPassword];
+
+  // check if all fields are filled out
+  for (var i = 0; i < inputs.length; i++) {
+    switch (i) {
+      case 0:
+        if (inputs[i] == '') {
+          return alert('Please enter your first name');
+        }
+        break;
+
+      case 1:
+        if (inputs[i] == '') {
+          return alert('Please enter your last name');
+        }
+        break;
+
+      case 2:
+        if (inputs[i] == '') {
+          return alert('Please enter your password');
+        }
+        break;
+
+      case 3:
+        if (inputs[i] == '') {
+          return alert('Please confirm your password');
+        }
+        break;
+    }
+  }
+
+  // confirm if passwords match
+  if (!(password === confirmPassword)) {
+    return alert('Your passwords do not match');
+  }
+
+  // send registration into to backend
+  $.ajax({
+    type: 'POST',
+    url: '/register',
+    data: {'firstName':firstName, 'lastName':lastName, 'email':email, 'password':password},
+    success: function (data) {
+      if (data.success) {
+        // window.location.href = '/';
+      }
+    }
+  })
+});
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('register', { title: 'Register' });
+});
+
+router.post('/', function (req, res, next) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  var password = req.body.password;
+
+  var saltRounds = 10;
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      // put hash into db
+      console.log(hash);
+      res.send({success: 'it worked'});
+    })
+  });
+  
 });
 
 module.exports = router;
